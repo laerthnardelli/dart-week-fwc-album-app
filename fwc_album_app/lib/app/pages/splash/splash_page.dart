d6c3555f32435adb38f57a97915739c82a41246d
laerthnardelli <laerthnardelli@gmail.com>
2022-10-10 22:00:00 -0300

    added helpers Loader

--- fwc_album_app/lib/app/pages/splash/splash_page.dart
+++ fwc_album_app/lib/app/pages/splash/splash_page.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:fwc_album_app/app/core/ui/helpers/loader.dart';
 
 import 'package:fwc_album_app/app/core/ui/styles/button_styles.dart';
 import 'package:fwc_album_app/app/core/ui/widgets/button.dart';
 import 'package:fwc_album_app/app/core/ui/styles/text_styles.dart';
 import 'package:fwc_album_app/app/core/ui/widgets/rounded_button.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> with Loader {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +25,12 @@ class SplashPage extends StatelessWidget {
           children: [
             ElevatedButton(
               style: ButtonStyles.i.yellowButton,
-              onPressed: () {},
+              onPressed: () async {
+                showLoader();
+
+                await Future.delayed(Duration(seconds: 2));
+                hideLoader();
+              },
               child: const Text('Salvar'),
             ),
             OutlinedButton(
