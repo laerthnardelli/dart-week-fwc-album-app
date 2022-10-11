511eaeadfb46024040cb060e952a603f5ba74551
laerthnardelli <laerthnardelli@gmail.com>
2022-10-11 15:03:05 -0300

    added config dotenv in project

--- fwc_album_app/lib/main.dart
+++ fwc_album_app/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:fwc_album_app/app/core/config/env/env.dart';
 import 'package:fwc_album_app/app/fwc_album_app.dart';
 
-void main() {
-  runApp(const FwcAlbumApp());
+void main() async {
+  await Env.i.load();
+  runApp(
+    const FwcAlbumApp(),
+  );
 }
