18f55885ca39bef37fc19d46c279dff1cad0f445
laerthnardelli <laerthnardelli@gmail.com>
2022-10-11 22:21:25 -0300

    implementation of user registration screen

---
+++ fwc_album_app/lib/app/pages/auth/register/presenter/register_presenter_impl.dart
@@ -0,0 +1,38 @@
+import 'dart:developer';
+
+import 'package:fwc_album_app/app/models/register_user_model.dart';
+import 'package:fwc_album_app/app/pages/auth/register/presenter/register_presenter.dart';
+import 'package:fwc_album_app/app/pages/auth/register/view/register_view.dart';
+import 'package:fwc_album_app/app/repository/auth_repository.dart';
+
+class RegisterPresenterImpl implements RegisterPresenter {
+  late final RegisterView _view;
+
+  final AuthRepository authRepository;
+
+  RegisterPresenterImpl({required this.authRepository});
+
+  @override
+  Future<void> register(
+      {required String name,
+      required String email,
+      required String password,
+      required String confirmPassword}) async {
+    final registerUserModel = RegisterUserModel(
+        name: name,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword);
+
+    try {
+      await authRepository.register(registerUserModel);
+      _view.registerSuccess();
+    } on Exception catch (e, s) {
+      log('Erro ao cadastrar usuário', error: e, stackTrace: s);
+      _view.registerError();
+    }
+  }
+
+  @override
+  set view(RegisterView view) => _view = view;
+}
--- fwc_album_app/lib/app/pages/auth/register/register_page.dart
+++ fwc_album_app/lib/app/pages/auth/register/register_page.dart
@@ -1,73 +1,137 @@
 import 'package:flutter/material.dart';
 import 'package:fwc_album_app/app/core/ui/styles/text_styles.dart';
 import 'package:fwc_album_app/app/core/ui/widgets/button.dart';
+import 'package:fwc_album_app/app/pages/auth/register/presenter/register_presenter.dart';
+import 'package:fwc_album_app/app/pages/auth/register/view/register_view_impl.dart';
+import 'package:validatorless/validatorless.dart';
 
-class RegisterPage extends StatelessWidget {
-  const RegisterPage({super.key});
+class RegisterPage extends StatefulWidget {
+  final RegisterPresenter presenter;
+
+  const RegisterPage({super.key, required this.presenter});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends RegisterViewImpl {
+  final formKey = GlobalKey<FormState>();
+
+  final nameEC = TextEditingController();
+  final emailEC = TextEditingController();
+  final passwordEC = TextEditingController();
+  final confirmPasswordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    nameEC.dispose();
+    emailEC.dispose();
+    passwordEC.dispose();
+    confirmPasswordEC.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 20),
-            Container(
-              height: 106.82,
-              padding: const EdgeInsets.all(10),
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                    'assets/images/bola.png',
+      body: Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              Container(
+                height: 106.82,
+                padding: const EdgeInsets.all(10),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/images/bola.png',
+                    ),
+                    fit: BoxFit.fill,
                   ),
-                  fit: BoxFit.fill,
                 ),
               ),
-            ),
-            const SizedBox(height: 50),
-            Text(
-              'Cadastrar Usuário',
-              style: context.textStyles.titleBlack,
-            ),
-            const SizedBox(height: 30),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text('Nome Completo *'),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text('E-mail*'),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text('Senha *'),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text('Confirma Senha *'),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Button.primary(
-                      width: MediaQuery.of(context).size.width * .9,
-                      onPressed: () {},
-                      label: 'Cadastrar')
-                ],
+              const SizedBox(height: 50),
+              Text(
+                'Cadastrar Usuário',
+                style: context.textStyles.titleBlack,
               ),
-            )
-          ],
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: nameEC,
+                      decoration: const InputDecoration(
+                        label: Text('Nome Completo *'),
+                      ),
+                      validator: Validatorless.required('Obrigatório'),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: emailEC,
+                      decoration: const InputDecoration(
+                        label: Text('E-mail*'),
+                      ),
+                      validator: Validatorless.multiple([
+                        Validatorless.required('Obrigatório'),
+                        Validatorless.email('E-mail inválido'),
+                      ]),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: passwordEC,
+                      decoration: const InputDecoration(
+                        label: Text('Senha *'),
+                      ),
+                      obscureText: true,
+                      validator: Validatorless.multiple([
+                        Validatorless.required('Obrigatório'),
+                        Validatorless.min(
+                            6, 'Senha deve conter pelo 6 caracteres'),
+                      ]),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: confirmPasswordEC,
+                      decoration: const InputDecoration(
+                        label: Text('Confirma Senha *'),
+                      ),
+                      obscureText: true,
+                      validator: Validatorless.multiple([
+                        Validatorless.required('Obrigatório'),
+                        Validatorless.min(
+                            6, 'Senha deve conter pelo 6 caracteres'),
+                        Validatorless.compare(
+                            passwordEC, 'Senha diferente de confirma senha')
+                      ]),
+                    ),
+                    const SizedBox(height: 20),
+                    Button.primary(
+                        width: MediaQuery.of(context).size.width * .9,
+                        onPressed: () {
+                          final formValid =
+                              formKey.currentState?.validate() ?? false;
+
+                          if (formValid) {
+                            showLoader();
+                            widget.presenter.register(
+                                name: nameEC.text,
+                                email: emailEC.text,
+                                password: passwordEC.text,
+                                confirmPassword: confirmPasswordEC.text);
+                          }
+                        },
+                        label: 'Cadastrar')
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
