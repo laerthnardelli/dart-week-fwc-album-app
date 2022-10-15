689b97462dc0367d728cd8085459a4456a4d7baa
laerthnardelli <laerthnardelli@gmail.com>
2022-10-15 16:28:29 -0300

    added service layer search stickers - FindStickerService and FindStickerServiceImpl

---
+++ fwc_album_app/lib/app/services/sticker/find_sticker_service_impl.dart
@@ -0,0 +1,28 @@
+import 'package:fwc_album_app/app/models/register_sticker_model.dart';
+import 'package:fwc_album_app/app/models/sticker_model.dart';
+import 'package:fwc_album_app/app/repository/stickers/stickers_repository.dart';
+
+import 'find_sticker_service.dart';
+
+class FindStickerServiceImpl implements FindStickerService {
+  final StickersRepository stickersRepository;
+
+  FindStickerServiceImpl({required this.stickersRepository});
+
+  @override
+  Future<StickerModel> execute(String counterCode, String stickerNumber) async {
+    var sticker =
+        await stickersRepository.findStickerByCode(counterCode, stickerNumber);
+
+    if (sticker == null) {
+      final registerSticker = RegisterStickerModel(
+        name: '',
+        stickerCode: counterCode,
+        stickerNumber: stickerNumber,
+      );
+      sticker = await stickersRepository.create(registerSticker);
+    }
+    return sticker;
+    //auto promoção de tipos
+  }
+}
